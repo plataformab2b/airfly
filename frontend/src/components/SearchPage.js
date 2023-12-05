5393b037b4ffc4d1a41ea04d2009c70c90a8357d
ExcelCall SA <renee_vera@excelcallsa.com>
2023-12-05 07:13:06 -0800

    new funtion

--- frontend/src/components/SearchPage.js
+++ frontend/src/components/SearchPage.js
@@ -133,115 +133,122 @@ const handlePassengersChange = (e) => {
   return (
     <div className="container mt-5">
       <h1 className="mb-4">Flight Booking System</h1>
-      <form onSubmit={handleSubmit}>
-  
-        {/* Dropdown to select the departure city */}
-        <div className="mb-3">
-          <label htmlFor="fromCity" className="form-label">From City</label>
-          <select className="form-select" name="fromCity" onChange={handleCityChange} value={search.fromCity}>
-            {airportsData.map((entry, index) => (
-              <option key={index} value={entry.city}>
-                {entry.city}
-              </option>
-            ))}
-          </select>
-        </div>
-  
-        {/* Dropdown to select the departure airport if a city has been selected */}
-        {search.fromCity && (
-          <div className="mb-3">
-            <label htmlFor="fromAirport" className="form-label">From Airport</label>
-            <select className="form-select" name="fromAirport" onChange={(e) => handleAirportChange(e, 'from')}>
-              {airportsData.find(city => city.city === search.fromCity).airports.map((airport, index) => (
-                <option key={index} value={airport.name}>
-                  {airport.name}
-                </option>
-              ))}
-            </select>
-          </div>
-        )}
-  
-        {/* Dropdown to select the destination city */}
-        <div className="mb-3">
-          <label htmlFor="toCity" className="form-label">To City</label>
-          <select className="form-select" name="toCity" onChange={handleCityChange} value={search.toCity}>
-            {airportsData.map((entry, index) => (
-              <option key={index} value={entry.city}>
-                {entry.city}
-              </option>
-            ))}
-          </select>
-        </div>
-  
-        {/* Dropdown to select the destination airport if a city has been selected */}
-        {search.toCity && (
-          <div className="mb-3">
-            <label htmlFor="toAirport" className="form-label">To Airport</label>
-            <select className="form-select" name="toAirport" onChange={(e) => handleAirportChange(e, 'to')}>
-              {airportsData.find(city => city.city === search.toCity).airports.map((airport, index) => (
-                <option key={index} value={airport.name}>
-                  {airport.name}
-                </option>
-              ))}
-            </select>
-          </div>
-        )}
-  
-        {/* Input to select the date */}
-        <div className="mb-3">
-          <label htmlFor="fromDate" className="form-label">Date</label>
-          <input type="date" className="form-control" name="fromDate" onChange={handleCityChange} value={search.fromDate} />
-        </div>
-  
-        {/* Input to enter the number of passengers */}
-        <div className="mb-3">
-          <label htmlFor="passengers" className="form-label">Number of Passengers</label>
-          <input type="number" className="form-control" name="passengers" onChange={handlePassengersChange} value={search.passengers} />
-        </div>
-  
-        {/* Button to submit the form */}
-        <button type="submit" className="btn btn-primary">Search</button>
-      </form>
-  
-      {/* Displaying the flight data in a table */}
-      {flightsData.length > 0 && (
-        <table className="table mt-4">
-          <thead>
-            <tr>
-              <th>Weekday</th>
-              <th>Departure</th>
-              <th>Arrival</th>
-              <th>Aircraft</th>
-              <th style={{width: '100px'}}>Book</th>
-              
-            </tr>
-          </thead>
-          <tbody>
-            {flightsData.map((flight, index) => (
-              <tr key={index}>
-                <td>{flight.weekday}</td>
-                <td>{`${flight.departure.iataCode} - ${flight.departure.scheduledTime}`}</td>
-                <td>{`${flight.arrival.iataCode} - ${flight.arrival.scheduledTime}`}</td>
-                <td>{flight.aircraft.modelText}</td>
-                <td>
-                {isAuthenticated() && (
-                  <button 
-                    className="btn btn-primary"
-                    style={{marginLeft: 'auto', display: 'block'}}  
-                    onClick={() => handleSelectBooking(flight)}>
-                  Select
-                  </button>
-                )}
-                </td>
+      {isAuthenticated() ? (
+        <>
+          {/* Form only shown if user is authenticated */}
+          <form onSubmit={handleSubmit}>
+            {/* Dropdown to select the departure city */}
+            <div className="mb-3">
+              <label htmlFor="fromCity" className="form-label">From City</label>
+              <select className="form-select" name="fromCity" onChange={handleCityChange} value={search.fromCity}>
+                {airportsData.map((entry, index) => (
+                  <option key={index} value={entry.city}>
+                    {entry.city}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/* Dropdown to select the departure airport if a city has been selected */}
+            {search.fromCity && (
+              <div className="mb-3">
+                <label htmlFor="fromAirport" className="form-label">From Airport</label>
+                <select className="form-select" name="fromAirport" onChange={(e) => handleAirportChange(e, 'from')}>
+                  {airportsData.find(city => city.city === search.fromCity).airports.map((airport, index) => (
+                    <option key={index} value={airport.name}>
+                      {airport.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            )}
+
+            {/* Dropdown to select the destination city */}
+            <div className="mb-3">
+              <label htmlFor="toCity" className="form-label">To City</label>
+              <select className="form-select" name="toCity" onChange={handleCityChange} value={search.toCity}>
+                {airportsData.map((entry, index) => (
+                  <option key={index} value={entry.city}>
+                    {entry.city}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            {/* Dropdown to select the destination airport if a city has been selected */}
+            {search.toCity && (
+              <div className="mb-3">
+                <label htmlFor="toAirport" className="form-label">To Airport</label>
+                <select className="form-select" name="toAirport" onChange={(e) => handleAirportChange(e, 'to')}>
+                  {airportsData.find(city => city.city === search.toCity).airports.map((airport, index) => (
+                    <option key={index} value={airport.name}>
+                      {airport.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            )}
+
+            {/* Input to select the date */}
+            <div className="mb-3">
+              <label htmlFor="fromDate" className="form-label">Date</label>
+              <input type="date" className="form-control" name="fromDate" onChange={handleCityChange} value={search.fromDate} />
+            </div>
+
+            {/* Input to enter the number of passengers */}
+            <div className="mb-3">
+              <label htmlFor="passengers" className="form-label">Number of Passengers</label>
+              <input type="number" className="form-control" name="passengers" onChange={handlePassengersChange} value={search.passengers} />
+            </div>
+
+            {/* Button to submit the form */}
+            {isAuthenticated() && (
+              <button type="submit" className="btn btn-primary">Search</button>
+            )}
+          </form>
+
+          {/* Displaying the flight data in a table */}
+          {flightsData.length > 0 && (
+            <table className="table mt-4">
+            <thead>
+              <tr>
+                <th>Weekday</th>
+                <th>Departure</th>
+                <th>Arrival</th>
+                <th>Aircraft</th>
+                <th style={{width: '100px'}}>Book</th>
+                
               </tr>
-            ))}
-          </tbody>
-        </table>
-        
+            </thead>
+            <tbody>
+              {flightsData.map((flight, index) => (
+                <tr key={index}>
+                  <td>{flight.weekday}</td>
+                  <td>{`${flight.departure.iataCode} - ${flight.departure.scheduledTime}`}</td>
+                  <td>{`${flight.arrival.iataCode} - ${flight.arrival.scheduledTime}`}</td>
+                  <td>{flight.aircraft.modelText}</td>
+                  <td>
+                  {isAuthenticated() && (
+                    <button 
+                      className="btn btn-primary"
+                      style={{marginLeft: 'auto', display: 'block'}}  
+                      onClick={() => handleSelectBooking(flight)}>
+                    Select
+                    </button>
+                  )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          )}
+        </>
+      ) : (
+        // Message shown if user is not authenticated
+        <p>User is not logged in</p>
       )}
     </div>
   );
-  
 };
 
 export default SearchPage;
